003db649266cfcbf3e06496ba3e16f48e82b20f9
Eliza Peterson <121064539+ElizaPeterson@users.noreply.github.com>
2023-02-01 23:00:38 +0000

    Advanced Extensions

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -13,10 +13,43 @@ def books():
     books = book_repository.select_all()
     return render_template("books/index.html", all_books=books)
 
+@books_blueprint.route("/books/new", methods=["GET"])
+def new_book():
+    authors = author_repository.select_all()
+    return render_template("books/new.html", all_authors=authors)
+
+@books_blueprint.route("/books", methods=["POST"])
+def create_book():
+    title = request.form["title"]
+    genre = request.form['genre']
+    description = request.form["description"]
+    author_id = request.form["author_id"]
+    author = author_repository.select(author_id)
+    book = Book(title, genre, description, author)
+    book_repository.save(book)
+    return redirect("/books")
+
 @books_blueprint.route("/books/<id>", methods=["GET"])
 def show_book(id):
     book = book_repository.select(id)
     return render_template('books/show.html', book=book)
+
+@books_blueprint.route("/books/<id>/edit", methods=["GET"])
+def edit_book(id):
+    book = book_repository.select(id)
+    authors = author_repository.select_all()
+    return render_template('books/edit.html', book = book, all_authors = authors)
+
+@books_blueprint.route("/books/<id>",  methods=["POST"])
+def update_book(id):
+    title = request.form["title"]
+    genre = request.form['genre']
+    description = request.form["description"]
+    author_id = request.form["author_id"]
+    author = author_repository.select(author_id)
+    book = Book(title, genre, description, author, id)
+    book_repository.update(book)
+    return redirect("/books")
 
 @books_blueprint.route("/books/<id>/delete", methods=["POST"])
 def delete_task(id):
